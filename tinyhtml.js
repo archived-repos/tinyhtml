2f391a50ac4424c761e648236da845cfe1494728
Jesús Germade <jesus@germade.es>
2017-04-28 14:23:24 +0200

    added umd wrapper

--- tinyhtml.js
+++ tinyhtml.js
@@ -1,72 +1,79 @@
 
-function byCamelCase (_matched, a, b) {
-  return a + b.toUpperCase();
-}
+(function (root, factory) {
+  if (typeof module === 'object' && module.exports) module.exports = factory();
+  else if (typeof define === 'function' && define.amd) define([], factory);
+  else root.tinyHTML = factory();
+})(this, function () {
 
-function attr2CamelCase (attrName) {
-  return attrName.replace(/([a-z])-([a-z])/g, byCamelCase);
-}
-
-function TagValues (attrs_str, body) {
-  this.attrs_str = attrs_str;
-  this.body = body;
-}
-
-Object.defineProperty(TagValues.prototype, 'attrs', {
-  get: function () {
-    if( !this.$$parsedAttributes ) this.$$parsedAttributes = this.attrs_str.match(/[a-z][a-z-]+(=".*?")?/g).reduce(function (attrs, attr) {
-      attr.replace(/([a-z][a-z-]+)(="(.*?)")?/, function (matched, name, hasValue, value) {
-        attrs[attr2CamelCase(name)] = hasValue ? value : '';
-      });
-      return attrs;
-    }, {});
-
-    return this.$$parsedAttributes;
+  function byCamelCase (_matched, a, b) {
+    return a + b.toUpperCase();
   }
-});
 
-var tagReplacer = {};
-function replaceTag (tagName, html, cb) {
-  if( !tagReplacer[tagName] ) tagReplacer[tagName] = new RegExp('<' + tagName + ' (.*?)>(.*?)</' + tagName + '>', 'g');
-  return html.replace(tagReplacer[tagName], function (_matched, attrs, body) {
-    var node = new TagValues(attrs, body),
-        result = cb(node);
+  function attr2CamelCase (attrName) {
+    return attrName.replace(/([a-z])-([a-z])/g, byCamelCase);
+  }
 
-    return '<' + tagName + ' ' + node.attrs_str + '>' + result + '</' + tagName + '>';
+  function TagValues (attrs_str, body) {
+    this.attrs_str = attrs_str;
+    this.body = body;
+  }
+
+  Object.defineProperty(TagValues.prototype, 'attrs', {
+    get: function () {
+      if( !this.$$parsedAttributes ) this.$$parsedAttributes = this.attrs_str.match(/[a-z][a-z-]+(=".*?")?/g).reduce(function (attrs, attr) {
+        attr.replace(/([a-z][a-z-]+)(="(.*?)")?/, function (matched, name, hasValue, value) {
+          attrs[attr2CamelCase(name)] = hasValue ? value : '';
+        });
+        return attrs;
+      }, {});
+
+      return this.$$parsedAttributes;
+    }
   });
-}
 
-function sanitizedAttribute (_matched, name, value, followsSpaces) {
-  if( name === 'style' ) value = value.replace(/([:;])\s+/g, '$1');
-  return name + '="' + value + '"' + (followsSpaces ? ' ' : '');
-}
+  var tagReplacer = {};
+  function replaceTag (tagName, html, cb) {
+    if( !tagReplacer[tagName] ) tagReplacer[tagName] = new RegExp('<' + tagName + ' (.*?)>(.*?)</' + tagName + '>', 'g');
+    return html.replace(tagReplacer[tagName], function (_matched, attrs, body) {
+      var node = new TagValues(attrs, body),
+          result = cb(node);
 
-function cleanTag (_matched, tag) {
-  return tag.replace(/([a-z][a-z-]+)="\s*(.*?)\s*"(\s+)?/g, sanitizedAttribute);
-}
-
-function tinyHTML (html, options) {
-  options = options || {};
-
-  var result = html.replace(/\n/g, '').replace(/\t+/g, ' ');
-
-  if( options.removeDoubleSpaces || options.removeDoubleSpaces === undefined ) {
-    result = result.replace(/\s+/g, ' ');
+      return '<' + tagName + ' ' + node.attrs_str + '>' + result + '</' + tagName + '>';
+    });
   }
 
-  if( options.removeComments || options.removeComments === undefined ) {
-    result = result.replace(/<!--[\s\S]+?-->/g, '');
+  function sanitizedAttribute (_matched, name, value, followsSpaces) {
+    if( name === 'style' ) value = value.replace(/([:;])\s+/g, '$1');
+    return name + '="' + value + '"' + (followsSpaces ? ' ' : '');
   }
 
-  if( options.parsers ) for( var tag in options.parsers ) {
-    result = replaceTag(tag, result, options.parsers[tag] );
+  function cleanTag (_matched, tag) {
+    return tag.replace(/([a-z][a-z-]+)="\s*(.*?)\s*"(\s+)?/g, sanitizedAttribute);
   }
 
-  result = result.replace(/\s*(<.*?>)\s*/g,
-    options.removeAttributeSpaces || options.removeAttributeSpaces === undefined ? cleanTag : '$1'
-  );
+  function tinyHTML (html, options) {
+    options = options || {};
 
-  return result;
-}
+    var result = html.replace(/\n/g, '').replace(/\t+/g, ' ');
 
-module.exports = tinyHTML;
+    if( options.removeDoubleSpaces || options.removeDoubleSpaces === undefined ) {
+      result = result.replace(/\s+/g, ' ');
+    }
+
+    if( options.removeComments || options.removeComments === undefined ) {
+      result = result.replace(/<!--[\s\S]+?-->/g, '');
+    }
+
+    if( options.parsers ) for( var tag in options.parsers ) {
+      result = replaceTag(tag, result, options.parsers[tag] );
+    }
+
+    result = result.replace(/\s*(<.*?>)\s*/g,
+      options.removeAttributeSpaces || options.removeAttributeSpaces === undefined ? cleanTag : '$1'
+    );
+
+    return result;
+  }
+
+  return tinyHTML;
+});
