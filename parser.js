35518224a6ad97e0fa3921f755d8e3d2bda61319
Jesús Germade <jesus@germade.es>
2018-06-25 13:23:27 +0200

    refactor for comments

--- parser.js
+++ parser.js
@@ -1,5 +1,5 @@
 
-function _parseTag (tag_str, options) {
+function _parseTag (tag_str, _options) {
   var tag = { attrs: {}, _: [], unclosed: true };
   // var tag = { _: [] };
       // tag = Object.create(tag_proto);
@@ -22,8 +22,8 @@ function _parseTag (tag_str, options) {
     })
     .replace(/ *([^=]+) *= *"([\s\S]*?)"| *([^=]+) *= *'([\s\S]*?)'/g, function (_matched, attribute, value) {
       attribute = attribute.trim();
-      if( attribute === 'style' ) value = value.replace(/([:;])\s+/g, '$1');
-      if( options.compress_attibutes !== false ) value = value.replace(/ *\n */g, '').trim();
+      // if( attribute === 'style' ) value = value.replace(/([:;])\s+/g, '$1');
+      // if( _options.compress_attibutes !== false ) value = value.replace(/ *\n */g, '').trim();
       tag.attrs[attribute] = value;
       // tag[attribute.trim()] = value;
       return '';
@@ -43,53 +43,143 @@ function _trimText (text) {
   return text.replace(/ *\n+ */g, '');
 }
 
-function _parseHTML (html, options) {
-  options = options || {};
+// function _parseHTML (html, options) {
+//   options = options || {};
+//
+//   // removing comments
+//   if( options.remove_comments !== false ) html = html.replace(/<!--([\s\S]+?)-->/g, '');
+//
+//   var contents = html.split(/<[^>]+?>/g);
+//   // var tags = html.match(/<([^>]+?)>/g);
+//   var nodes = options.nodes || [],
+//       node_opened = options.node_opened || { $: '__root__', _: nodes };
+//
+//   (html.match(/<([^>]+?)>/g) ||[]).map(function (tag, i) {
+//
+//     // if( /^\s+$/.test(contents[i]) ) contents[i] = '';
+//
+//     // if( i && contents[i] && !/^\s+$/.test(contents[i]) ) node_opened._.push({
+//     if( /\S/.test(contents[i]) ) node_opened._.push({
+//       text: _trimText(contents[i]),
+//     });
+//
+//     tag = _parseTag(tag, options);
+//
+//     if( tag.closer ) {
+//       if( tag.$ !== node_opened.$ ) throw new Error('tag closer \'' + tag.$ + '\' for \'' + node_opened.$ + '\'' );
+//       delete node_opened.unclosed;
+//       node_opened = node_opened._parent;
+//     } else if( tag.self_closed ) {
+//       node_opened._.push(tag);
+//     } else {
+//       tag._parent = node_opened;
+//       // tag._ = [];
+//       node_opened._.push(tag);
+//       node_opened = tag;
+//     }
+//
+//     return tag;
+//
+//   });
+//
+//   if( /\S$/.test(contents[contents.length - 1]) ) nodes.push({
+//     text: _trimText(contents[contents.length - 1]),
+//   });
+//
+//   return {
+//     nodes: nodes,
+//     node_opened: node_opened,
+//   };
+// }
+//
+// var full_content_tags = [
+//   'script',
+//   'style',
+//   'code',
+// ];
+//
+// var RE_full_content = new RegExp( full_content_tags.map(function (tag_name) {
+//   return '<' + tag_name + '[^>]*>|<\\/' + tag_name + '>';
+// }).join('|'), 'g');
+//
+// // var RE_full_content = new RegExp('<(' + full_content_tags.join('|') + ')[^>]*>|<\\/(' + full_content_tags.join('|') + ')>', 'g');
+//
+// function _cleanNodes (nodes) {
+//   nodes.forEach(function (tag) {
+//     // avoiding circular structure
+//     delete tag._parent;
+//
+//     // cleaning empty attributes
+//     if( tag.attrs && Object.keys(tag.attrs).length === 0 ) delete tag.attrs;
+//
+//     // cleaning empty children
+//     if( tag._ instanceof Array ) {
+//       if( !tag._.length ) delete tag._;
+//       else _cleanNodes(tag._);
+//     }
+//
+//   });
+//   return nodes;
+// }
+//
+// // @TODO: parse comments first of all
+//
+// function parseHTML (html, options) {
+//   var tags = html.match(RE_full_content) ||[],
+//       contents = html.split(RE_full_content),
+//       tag_opened = null,
+//       nodes = [],
+//       last_parse = {};
+//
+//   options = options || {};
+//
+//   tags.forEach(function (tag, i) {
+//
+//     if( tag_opened ) tag_opened._ = contents[i];
+//     else last_parse = _parseHTML(contents[i], {
+//       nodes: nodes,
+//       node_opened: last_parse.node_opened,
+//       remove_comments: options.remove_comments,
+//       compress_attibutes: options.compress_attibutes
+//     });
+//
+//     tag = _parseTag(tag, options);
+//
+//     if( tag.closer ) {
+//       if( !tag_opened || tag_opened.$ !== tag.$ ) throw new Error('tag closer \'' + tag.$ + '\' for \'' + (tag_opened ? tag_opened.$ : '!tag_opened') + '\'' );
+//       delete tag_opened.unclosed;
+//       tag_opened = null;
+//     } else {
+//       tag_opened = tag;
+//       (last_parse.node_opened ? last_parse.node_opened._ : nodes).push(tag);
+//     }
+//
+//   });
+//
+//   last_parse = _parseHTML(contents[contents.length - 1], {
+//     nodes: nodes,
+//     node_opened: last_parse.node_opened,
+//   });
+//
+//   if( !options.ignore_unclosed && last_parse.node_opened && last_parse.node_opened.unclosed && !last_parse.node_opened.warn ) {
+//     throw new Error('tab unclosed \'' + last_parse.node_opened.$ + '\'');
+//   }
+//
+//   return _cleanNodes(nodes);
+// }
 
-  // removing comments
-  if( options.remove_comments !== false ) html = html.replace(/<!--([\s\S]+?)-->/g, '');
+function _zipTokens (splitted, matched) {
+  var list = [];
+  for( var i = 0, n = matched.length ; i < n ; i++ ) {
+    list.push( splitted[i] );
+    list.push( matched[i] );
+  }
+  list.push( matched[i] );
+  return list;
+}
 
-  var contents = html.split(/<[^>]+?>/g);
-  // var tags = html.match(/<([^>]+?)>/g);
-  var nodes = options.nodes || [],
-      node_opened = options.node_opened || { $: '__root__', _: nodes };
-
-  (html.match(/<([^>]+?)>/g) ||[]).map(function (tag, i) {
-
-    // if( /^\s+$/.test(contents[i]) ) contents[i] = '';
-
-    // if( i && contents[i] && !/^\s+$/.test(contents[i]) ) node_opened._.push({
-    if( /\S/.test(contents[i]) ) node_opened._.push({
-      text: _trimText(contents[i]),
-    });
-
-    tag = _parseTag(tag, options);
-
-    if( tag.closer ) {
-      if( tag.$ !== node_opened.$ ) throw new Error('tag closer \'' + tag.$ + '\' for \'' + node_opened.$ + '\'' );
-      delete node_opened.unclosed;
-      node_opened = node_opened._parent;
-    } else if( tag.self_closed ) {
-      node_opened._.push(tag);
-    } else {
-      tag._parent = node_opened;
-      // tag._ = [];
-      node_opened._.push(tag);
-      node_opened = tag;
-    }
-
-    return tag;
-
-  });
-
-  if( /\S$/.test(contents[contents.length - 1]) ) nodes.push({
-    text: _trimText(contents[contents.length - 1]),
-  });
-
-  return {
-    nodes: nodes,
-    node_opened: node_opened,
-  };
+function _tokenize (text, RE_split, RE_match, mapTokens) {
+  return _zipTokens( text.split(RE_split), mapTokens ? text.match(RE_match).map(mapTokens) : text.match(RE_match) );
 }
 
 var full_content_tags = [
@@ -98,89 +188,20 @@ var full_content_tags = [
   'code',
 ];
 
-var RE_full_content = new RegExp(full_content_tags.map(function (tag_name) {
+var RE_full_content = new RegExp( full_content_tags.map(function (tag_name) {
   return '<' + tag_name + '[^>]*>|<\\/' + tag_name + '>';
 }).join('|'), 'g');
 
-// var RE_full_content = new RegExp('<(' + full_content_tags.join('|') + ')[^>]*>|<\\/(' + full_content_tags.join('|') + ')>', 'g');
+function _parseHTML (html, options) {
+  var tokens = _tokenize(html, RE_full_content, RE_full_content, _parseTag ), i, n;
 
-function _cleanNodes (nodes) {
-  nodes.forEach(function (tag) {
-    // avoiding circular structure
-    delete tag._parent;
-
-    // cleaning empty attributes
-    if( tag.attrs && Object.keys(tag.attrs).length === 0 ) delete tag.attrs;
-
-    // cleaning empty children
-    if( tag._ instanceof Array ) {
-      if( !tag._.length ) delete tag._;
-      else _cleanNodes(tag._);
-    }
-
-  });
-  return nodes;
-}
-
-// @TODO: parse comments first of all
-
-function parseHTML (html, options) {
-  var tags = html.match(RE_full_content) ||[],
-      contents = html.split(RE_full_content),
-      tag_opened = null,
-      nodes = [],
-      last_parse = {};
-
-  options = options || {};
-
-  tags.forEach(function (tag, i) {
-
-    if( tag_opened ) tag_opened._ = contents[i];
-    else last_parse = _parseHTML(contents[i], {
-      nodes: nodes,
-      node_opened: last_parse.node_opened,
-      remove_comments: options.remove_comments,
-      compress_attibutes: options.compress_attibutes
-    });
-
-    // else (function (parse_result) {
-    //   last_parse = parse_result;
-    // })( _parseHTML(contents[i], {
-    //   nodes: nodes,
-    //   node_opened: last_parse.node_opened,
-    // }) );
-
-    tag = _parseTag(tag, options);
-
-    if( tag.closer ) {
-      if( !tag_opened || tag_opened.$ !== tag.$ ) throw new Error('tag closer \'' + tag.$ + '\' for \'' + (tag_opened ? tag_opened.$ : '!tag_opened') + '\'' );
-      delete tag_opened.unclosed;
-      tag_opened = null;
-    } else {
-      tag_opened = tag;
-      (last_parse.node_opened ? last_parse.node_opened._ : nodes).push(tag);
-
-      // if( last_parse.node_opened ) {
-      //   last_parse.node_opened._ = last_parse.node_opened._ || [];
-      //   last_parse.node_opened._.push(tag);
-      // } else {
-      //   nodes.push(tag);
-      // }
-    }
-
-  });
-
-  last_parse = _parseHTML(contents[contents.length - 1], {
-    nodes: nodes,
-    node_opened: last_parse.node_opened,
-  });
-
-  // if( !options.ignore_unclosed && last_parse.node_opened && last_parse.node_opened.$ !== '__root__' && !last_parse.node_opened.warn ) {
-  if( !options.ignore_unclosed && last_parse.node_opened && last_parse.node_opened.unclosed && !last_parse.node_opened.warn ) {
-    throw new Error('tab unclosed \'' + last_parse.node_opened.$ + '\'');
+  for( i = 0, n = tokens.length; i < n ; i++ ) {
+    if( typeof tokens[i] === 'string' ) tokens[i] = _tokenize(tokens[i])
   }
-
-  return _cleanNodes(nodes);
 }
 
-module.exports = parseHTML;
+
+module.exports = function (html, options) {
+  options = options || {};
+  return _parseHTML(html, options);
+};
--- tests/parsing-tests.js
+++ tests/parsing-tests.js
@@ -28,6 +28,16 @@ describe('parser', function () {
 
   });
 
+  it('comments', function () {
+
+    assert.deepEqual( parseHTML(`
+<div id="foobar"
+     foo="bar">foo</div>
+<!--<div id="barfoo"
+    bar="foo">bar</div>-->    `), [{ $:'div', attrs:{ id: 'foobar', foo: 'bar' }, _:[{ text: 'foo' }] }] );
+
+  });
+
   it('script', function () {
 
     assert.deepEqual( parseHTML(`
@@ -36,6 +46,53 @@ describe('parser', function () {
 </script>
     `), [{ $:'script', attrs: { 'template:type': 'text/javascript' }, _:`
   var foo = 'bar';
+` }] );
+
+  });
+
+  it('div script', function () {
+
+    assert.deepEqual( parseHTML(`
+<div class="script-wrapper">
+<script template:type="text/javascript">
+  var foo = 'bar';
+</script>
+</div>
+    `), [{
+  $: 'div',
+  attrs: { class: 'script-wrapper' },
+  _: [{ $:'script', attrs: { 'template:type': 'text/javascript' }, _:`
+var foo = 'bar';
+` }]
+}] );
+
+  });
+
+  it('script comments (in)', function () {
+
+    assert.deepEqual( parseHTML(`
+<script template:type="text/html">
+  <div></div>
+  <!-- <div></div> -->
+</script>
+    `), [{ $:'script', attrs: { 'template:type': 'text/html' }, _:`
+  <div></div>
+  <!-- <div></div> -->
+` }] );
+
+  });
+
+  it('script comments (out)', function () {
+
+    assert.deepEqual( parseHTML(`
+<!--<script template:type="text/javascript">
+  var foo = 'bar';
+</script>-->
+<script template:type="text/javascript">
+  var bar = 'foo';
+</script>
+    `), [{ $:'script', attrs: { 'template:type': 'text/javascript' }, _:`
+  var bar = 'foo';
 ` }] );
 
   });
