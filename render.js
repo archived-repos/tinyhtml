1a5d354e07b6028514630ba7ae5aff819e242714
Jesús Germade <jesus@germade.es>
2018-06-25 11:43:36 +0200

    render tweaks

--- render.js
+++ render.js
@@ -1,22 +1,35 @@
 
-function _appendChildren (node, nodes) {
-  var node_el;
+function _appendChildren (parent, nodes) {
+  var node, node_el;
   for( var i = 0, n = nodes.length ; i < n ; i++ ) {
-    node_el = _create(nodes[i]);
-    node.appendChild( node_el );
-    if( nodes[i]._init instanceof Array ) nodes[i]._init.forEach(function (initFn) {
+    node = nodes[i];
+    node_el = _create(node, parent);
+    // console.log('_create', nodes[i], node_el, node);
+    parent.appendChild( node_el );
+    if( node._init instanceof Array ) node._init.forEach(function (initFn) {
       initFn.call(node_el);
     });
   }
 }
 
-function _create(node) {
-  var node_el = document.createElement(node.$);
-  if( node.attrs ) for( var key in node.attrs ) node_el.setAttribute(key, node.attrs[key]);
-  if( node._ instanceof Array ) _appendChildren(node_el, node._);
-  else if( node._ ) node_el.textContent = node._;
+function _create(node, _parent) {
+  var node_el;
+  if( node.$ ) {
+    node_el = document.createElement(node.$);
+    if( node.attrs ) {
+      for( var key in node.attrs ) node_el.setAttribute(key, node.attrs[key]);
+    }
+
+    // console.log('_create:children %c' + node.$, 'color: blue; font-weight: bold', node._ instanceof Array, node._ );
+    if( node._ instanceof Array ) _appendChildren(node_el, node._);
+    else if( node._ ) node_el.textContent = node._;
+  } else if( node.text ) return document.createTextNode(node.text);
+  // console.log('_create %c' + node.$, 'color: brown; font-weight: bold', node, node_el.outerHTML );
+
+  return node_el;
 }
 
 module.exports = function renderNodes (parent, nodes) {
+  // console.log('renderNodes', parent, nodes);
   _appendChildren(parent, nodes);
 };
